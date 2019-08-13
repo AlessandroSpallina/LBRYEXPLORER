4429b38287dc6c97e81cb583e67ae0a45c8a49bc
AlessandroSpallina <alessandrospallina1@gmail.com>
2019-08-13 18:00:30 +0200

    fix mobile wrong table visualization in transactions/blocks/mempool

--- src/resources/views/transactions.blade.php
+++ src/resources/views/transactions.blade.php
@@ -2,16 +2,26 @@
 
 @push('styles')
 <style>
-  .pagination.center,
-  .pagination.center ul {
-    float: left;
+  @media screen and (min-width: 1400px) {
+    .pagination.center,
+    .pagination.center ul {
+      float: left;
+      position: relative;
+    }
+    .pagination.center {
+      left: 50%;
+    }
+    .pagination.center ul {
+      left: -50%;
+    }
+  }
+
+  .my-custom-scrollbar {
     position: relative;
+    overflow: auto;
   }
-  .pagination.center {
-    left: 50%;
-  }
-  .pagination.center ul {
-    left: -50%;
+  .table-wrapper-scroll-y {
+    display: block;
   }
 </style>
 @endpush
@@ -23,7 +33,7 @@
 <div class="row">
   <div class="col-lg-12">
     <div class="main-card mb-3 card">
-      <div class="card-body">
+      <div class="card-body table-wrapper-scroll-y my-custom-scrollbar">
         <h5 class="card-title">Mined Transactions</h5>
         <table class="mb-0 table table-hover table-striped">
           <thead>
@@ -52,7 +62,7 @@
           </tbody>
         </table>
       </div>
-      <div class="card-footer">
+      <div class="card-footer table-wrapper-scroll-y my-custom-scrollbar">
           <div class="pagination center">
             {{ $transactions->links() }}
           </div>
